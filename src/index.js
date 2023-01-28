f399f61d363446fc393b2c02ce76ef3a230e4e5c
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2023-01-28 21:49:31 +0200

    up0.1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from 'components/App';
+import './index.css';
 
-ReactDOM.render(
-  <>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
